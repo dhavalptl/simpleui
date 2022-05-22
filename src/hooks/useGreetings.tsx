eca44d75bee079ed06f80d90c22a7ad63c447e83
Dhaval <dhvl.ptl123@yahoo.in>
2022-05-22 16:38:40 +0530

    Mock api

--- src/hooks/useGreetings.tsx
+++ src/hooks/useGreetings.tsx
@@ -2,5 +2,5 @@ import useSWR from "swr";
 import { fetcher } from "../utils/fetcher";
 
 export function useGreetings() {
-    return useSWR('/greetings', fetcher);
+    return useSWR('http://apiserver:4000/greetings', fetcher);
 }
--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,7 +1,7 @@
 import { rest } from 'msw'
 
 export const handlers = [
-    rest.get('/greetings', (req, res, ctx) => {
+    rest.get('http://apiserver:4000/greetings', (req, res, ctx) => {
       return res(ctx.status(200), ctx.json({
           message: "Welcome to application",
           date: new Date().toLocaleString()
