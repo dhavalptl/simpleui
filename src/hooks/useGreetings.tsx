5589f948c82c8277c68759e3e0321a93f4bccfb3
Dhaval <dhvl.ptl123@yahoo.in>
2022-05-28 22:44:46 +0530

    change api url

--- src/hooks/useGreetings.tsx
+++ src/hooks/useGreetings.tsx
@@ -2,5 +2,5 @@ import useSWR from "swr";
 import { fetcher } from "../utils/fetcher";
 
 export function useGreetings() {
-    return useSWR('/api/greetings', fetcher);
+    return useSWR('/greetings', fetcher);
 }
